cf6aa0ab191d79fddb7c05e1f74c0c64caf980a9
Kevin <kevinrisqi22@gmail.com>
2019-09-04 00:27:12 +0700

    Menambahkan method random dan variabel baru

--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -13,21 +13,28 @@ import java.util.Random;
 public class MainActivity extends AppCompatActivity {
 
 	//  TODO: deklarasikan variabel di sini
+	private EditText numberInput;
+	private int random;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		// TODO: bind layout di sini
+		numberInput = findViewById(R.id.number_input);
+		initRandomNumber();
 	}
 
 	// TODO: generate angka random di sini
 	private void initRandomNumber() {
-
+		final int min = 20;
+		final int max = 80;
+		random = new Random().nextInt((max - min) + 1) + min;
 	}
 
 	public void handleGuess(View view) {
 		// TODO: Tambahkan logika untuk melakukan pengecekan angka
+		numberInput = Integer.valueOf(numberInput)
 	}
 
 	public void handleReset(View view) {
