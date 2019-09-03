b6873e43579aee5a7e8b0c925a5d1d1693f8a060
Kevin <kevinrisqi22@gmail.com>
2019-09-03 15:05:10 +0700

    Penambahan Method start dan stop

--- AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
+++ AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
@@ -3,6 +3,7 @@ package id.ac.polinema.androidlifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,9 +14,17 @@ public class MainActivity extends AppCompatActivity {
 	}
 
 	// TODO: tambahkan callback onStart() di sini
-
+	@Override
+	protected void onStart(){
+		super.onstart();
+		Toast.makeText(context: this, text: "App on Start", Toast.LENGTH_SHORT).show();
+	}
 
 	// TODO: tambahkan callback onStop() di sini
-
+	@Override
+	protected void onStop(){
+		super.onStop();
+		Toast.makeText(context: this, text: "App on Stop", Toast.LENGTH_SHORT).show();
+	}
 	// TODO: lengkapi callback untuk onRestart() onResume() onPause() onDestroy()
 }
